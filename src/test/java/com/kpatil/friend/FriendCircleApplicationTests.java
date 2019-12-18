03682141c435bb722e9b8c52d2dcf9e7b6c3339d
kpatil <kpatil@salesforce.com>
2019-12-18 11:58:49 -0800

    controller unit test and system tests

--- src/test/java/com/kpatil/friend/FriendCircleApplicationTests.java
+++ src/test/java/com/kpatil/friend/FriendCircleApplicationTests.java
@@ -1,13 +1,23 @@
 package com.kpatil.friend;
 
+import com.kpatil.friend.controller.FriendController;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class FriendCircleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  private FriendController friendController;
+
+  @Test
+  void contextLoads() {
+    Assert.assertNotNull(friendController);
+  }
 
 }
