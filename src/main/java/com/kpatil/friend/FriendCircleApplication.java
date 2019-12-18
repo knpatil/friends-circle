2f70c2724f86ca0b8694e0e49d4e80e3f961fead
kpatil <kpatil@salesforce.com>
2019-12-17 16:46:58 -0800

    first commit

---
+++ src/main/java/com/kpatil/friend/FriendCircleApplication.java
@@ -0,0 +1,13 @@
+package com.kpatil.friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendCircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendCircleApplication.class, args);
+	}
+
+}
