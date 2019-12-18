810c3fa46baa7eca324a833c28dddeb08076a545
kpatil <kpatil@salesforce.com>
2019-12-17 21:55:57 -0800

    implemented search: findById and findByQuery

--- src/main/java/com/kpatil/friend/controller/FriendController.java
+++ src/main/java/com/kpatil/friend/controller/FriendController.java
@@ -2,6 +2,7 @@ package com.kpatil.friend.controller;
 
 import com.kpatil.friend.model.Friend;
 import com.kpatil.friend.service.FriendService;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -40,5 +42,27 @@ public class FriendController {
   void delete(@PathVariable Integer id) {
     friendService.deleteById(id);
   }
+
+  @GetMapping("/friends/{id}")
+  Optional<Friend> findById(@PathVariable Integer id) {
+    return friendService.findById(id);
+  }
+
+  @GetMapping("/friends/search")
+  Iterable<Friend> findByQuery(@RequestParam(value = "first", required = false) String firstName,
+      @RequestParam(value = "last", required = false) String lastName) {
+
+    if (firstName != null && lastName != null) {
+      return friendService.findByFirstNameAndLastName(firstName, lastName);
+    } else if (firstName != null) {
+      return friendService.findByFirstName(firstName);
+    } else if (lastName != null) {
+      return friendService.findByLastName(lastName);
+    } else {
+      return friendService.findAll();
+    }
+
+  }
+
 
 }
